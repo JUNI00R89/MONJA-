19a1a33bdf841d62365a0e27722c7f8b3a9fa660
frank <frankjunior5400@gmail.com>
2025-11-03 17:43:27 -0500

    Primer commit

---
+++ fattyy.js
@@ -0,0 +1,7 @@
+let numero1= parseFloat(document.getElementById("").value);
+let numero2 = parseFloat(document.getElementById("").value);
+let resultado = numero1 + numero2;  
+
+return resultado;
+
+console.log("El resultado de la suma es: " ,  resultado);
