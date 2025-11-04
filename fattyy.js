a78b278ef25dbffdbb1384161591699a1eb203ef
Frank junior benitez mosquera <frankjunior5400@gmail.com>
2025-11-03 19:46:02 -0500

    Delete fattyy.js

--- fattyy.js
+++
@@ -1,7 +0,0 @@
-let numero1= parseFloat(document.getElementById("").value);
-let numero2 = parseFloat(document.getElementById("").value);
-let resultado = numero1 + numero2;  
-
-return resultado;
-
-console.log("El resultado de la suma es: " ,  resultado);
