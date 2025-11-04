e046af871508630ff24a03823c1037369f06af4b
frank <frankjunior5400@gmail.com>
2025-11-03 19:32:17 -0500

    feat(calc): add sum function

---
+++ calculator.js
@@ -0,0 +1,9 @@
+
+
+let numero1= parseFloat(document.getElementById("").value);
+let numero2 = parseFloat(document.getElementById("").value);
+let resultado = numero1 + numero2;  
+
+return resultado;
+
+console.log("El resultado de la suma es: " + resultado);
